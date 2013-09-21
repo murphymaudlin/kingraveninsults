b274140d043943fd74c5c2676897c21bb19edefa
tristanpendergrass <tristanpendergrass@gmail.com>
2013-09-21 10:08:40 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kingraveninsults::Application.config.secret_token = '0c77c6b01d790031d75de399cedafa3550af40fe23a9e9e1518d20536fc0cbadf7541c84fea858c8247819c8c246eb59bbbcc9694bd0d7461c3a6f76e07adfb1'
