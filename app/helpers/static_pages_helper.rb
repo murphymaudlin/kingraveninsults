2730a52a0cbaa3c2678e7b663ef9f38ba3bd1b00
tristanpendergrass <tristanpendergrass@gmail.com>
2013-09-21 21:05:33 -0400

    Fixes some bugs and adds a slew of additional hard coded parameters

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,6 +1,7 @@
 
 module StaticPagesHelper
-  MAX_LENGTH = 50
+  MAX_LENGTH = 200
+  PREVENT_REPEATS = true
   
   CODE_MEANINGS = {
     "<N>" => :noun,
@@ -11,38 +12,74 @@ module StaticPagesHelper
   }
   
   NOUNS = {
-    "fuck" => 0,
-    "dumpster" => 0
+    "bad" => nil,
+    "plant" => nil,
+    "planter" => nil,
+    "dumpster" => nil,
+    "fat" => nil,
+    "kid" => nil,
+    "nerd" => nil,
+    "garbage" => nil,
+    "trash" => nil,
+    "scum" => nil,
+    "pub" => nil,
+    "clown" => nil,
+    "fuck" => nil,
+    "mad" => nil,
+    "feeder" => nil,
+    "bitch" => nil,
+    "ass" => nil
   }
   
   VERBS = {
+    "wrecked" => nil,
+    "blasted" => nil,
+    "rolled" => nil,
+    "shitted" => nil,
     "fucked" => nil,
-    "blasted" => nil
+    "licked" => nil
   }
   
   ADJECTIVES = {
     "planting" => nil,
     "fucking" => nil,
-    "typical" => nil
+    "typical" => nil,
+    "feeding" => nil,
+    "huge" => nil,
+    "mammoth" => nil,
+    "brazilian" => nil,
+    "massive" => nil,
+    "incredible" => nil,
+    "shitty" => nil
   }
     
   NOUN_PHRASE_TEMPLATES = {
-    "<N>"     => 50,
-    "<N><N>"     => 25,
-    "<NP> <NP>" => 5,
-    "<A> <NP>" => 50
+    "<N>"     => nil,
+    "<N><N>"     => nil,
+    "<NP> <NP>" => 50,
+    "<A> <NP>" => nil
   }
   
   VERB_PHRASE_TEMPLATES = {
     "<V>" => nil,
-    "<N><V>" => nil
+    "<V> on" => nil,
+    "<N><V>" => nil,
+    "<VP> and <VP>" => 10
   }
   
   INSULT_TEMPLATES = {
     "You're a <NP>" => nil,
+    "You're a <NP> and a <NP>" => nil,
+    "You're a <NP> and a <NP>, not to mention a <NP>" => 5,
     "You're just a <NP>" => nil,
+    "Such a <NP>" => nil,
+    "Get <VP>" => nil,
     "Get <VP>, <NP>" => nil,
-    "Get <VP> on, <NP>" => nil,
+    "Get <VP>, you <NP>" => nil,
+    "You're like a <NP> getting <VP>" => 2,
+    "You get <VP> every day of the week you <NP>" => 2,
+    "Don't forget to get <VP>, <NP>" => 2,
+    "Typical <NP>" => 200
   }
   
   def noun
@@ -71,19 +108,18 @@ module StaticPagesHelper
   end
   
   def evaluate_template(template)
-    puts "entering... (template = #{template})"
     begin
       return template if no_codes?(template)
     
       result = template.dup
       CODE_MEANINGS.each do |code, method_name|
-        raise if result.length > MAX_LENGTH
+        raise "too long" if result.length > MAX_LENGTH
         result.gsub!(code) { |c| send(method_name) } if result.match(code)
       end
-    
+      
+      raise "repeated words" if PREVENT_REPEATS && repeated_words?(result)
       result
     rescue
-      puts "rescued!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
       return evaluate_template(template)
     end
   end
@@ -95,20 +131,31 @@ module StaticPagesHelper
   def weighted_sample(weighted_hash)
     return weighted_hash.keys.sample if weighted_hash.values.uniq.size == 1
     
-    zeroed_weighted_hash = {}.tap do |zeroed_weighted_hash| 
+    leveled_weighted_hash = {}.tap do |result| 
       weighted_hash.each do |k, v|
-        zeroed_weighted_hash[k] = weighted_hash[k] == nil ? 0 : weighted_hash[k]
+        result[k] = weighted_hash[k] || 100
       end
     end
     
-    probabilities_sum = zeroed_weighted_hash.values.inject(&:+)
+    probabilities_sum = leveled_weighted_hash.values.inject(&:+)
     
-    pruned_weighted_hash = zeroed_weighted_hash.select { |_, p| p > 0 }
-    
+    pruned_weighted_hash = leveled_weighted_hash.select { |_, p| p > 0 }
     pruned_weighted_hash.each do |code, p|
       return code if p > rand(probabilities_sum)
       probabilities_sum -= p
     end
+  end
+  
+  def repeated_words?(sentence)
+    words = sentence.split(' ')
+    
+    i = 0
+    while i < words.length - 1
+      return true if words[i] == words[i + 1]
+      
+      i += 1
+    end
+    false
   end
 end
 
